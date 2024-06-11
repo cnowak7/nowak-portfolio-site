aac25e7d34c922eea5e9b5f55b5ee84b2256a77e
cnowak7 <rafi.slapass@gmail.com>
2024-06-10 22:26:18 -0500

    make copyright text smaller

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-black small text-center text-white-50">
-      <div className="container">
+    <footer className="bg-black fa-xs text-center text-white-50">
+      <div className="container fa-xs">
         Copyright &copy; Gatsby-starter-grayscale {new Date().getFullYear()}
       </div>
     </footer>
