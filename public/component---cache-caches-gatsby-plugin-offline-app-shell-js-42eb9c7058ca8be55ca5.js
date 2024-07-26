d888df39be7dd67f5c6d1bad86d926894295b4e3
cnowak7 <rafi.slapass@gmail.com>
2024-07-26 14:38:54 -0500

    Updates

--- public/component---cache-caches-gatsby-plugin-offline-app-shell-js-42eb9c7058ca8be55ca5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunknowak_portfolio_site=self.webpackChunknowak_portfolio_site||[]).push([[681],{7541:function(e,t,n){var u=n(4994);t.__esModule=!0,t.default=void 0;var o=u(n(6221)),r=u(n(6540)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),l=a;t.default=l}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-42eb9c7058ca8be55ca5.js.map
--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-367c03cd9b545a8230fc.js"
+    "url": "webpack-runtime-ae334eebbe0c2de43ad4.js"
   },
   {
     "url": "framework-3e21018dbf8d99d91e2c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b2d21393df4ca8951d40a169321ef389"
+    "revision": "4a07a02601d6ff62a085619bba8795f7"
   },
   {
     "url": "manifest.webmanifest",
